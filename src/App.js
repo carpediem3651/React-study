c13cf5e4e501bf0cbf1f34b910259f473bad06ae
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-02 16:19:19 +0900

    props 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
   return (
     <div className="App">
@@ -17,32 +18,20 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
-      <button onClick={()=>{
-        let copy = [...글제목];
-        copy.sort();
-        글제목변경(copy);
-      }}>글제목 가나다순 정렬</button>
-
-      <button onClick={()=>{
-        let copy = [...글제목];
-        copy[0] = '여자코트 추천';
-        글제목변경(copy);
-      }}>글수정</button>
-
-      <div className='list'>
-        <h4>{글제목[0]} <span onClick={() => {따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className='list'>
-        <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
       {
-        modal == true ? <Modal 글제목={글제목}/> : null
+        글제목.map(function(a,i){
+          return (
+            <div className='list'>
+              <h4 onClick={()=>{setModal(true); setTitle(i)}}>{글제목[i]}
+              <span onClick={()=>{ 따봉변경(따봉+1)}}>👍</span>{따봉}</h4>
+              <p>2월 17일 발행</p>
+            </div>
+          )
+        })
+      }
+
+      {
+        modal == true ? <Modal title={title} 글제목={글제목}/> : null
       }
     </div>
   );
@@ -51,9 +40,10 @@ function App() {
 function Modal(props) {
   return(
     <div className='modal'>
-      <h4>{props.글제목}</h4>
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   )
 }
